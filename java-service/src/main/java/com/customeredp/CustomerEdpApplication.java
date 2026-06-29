aef0dadf3d9afc0c70e6a83aaf204040e9c91aef
leyterhs <leyterhs@hotmail.com>
2026-06-30 00:44:41 +0300

    Phase 2: Added Java Service (Spring Boot) - initial structure

---
+++ java-service/src/main/java/com/customeredp/CustomerEdpApplication.java
@@ -0,0 +1,11 @@
+package com.customeredp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerEdpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerEdpApplication.class, args);
+    }
+}
